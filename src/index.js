b12e40929684ae53d24d33c9ac9be791aa6e6678
Jose David <jose7312000@gmail.com>
2022-11-09 15:50:14 -0500

    Agregando conexion a la db

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import app from './app'
 import morgan from 'morgan'
 import productRoutes from './routes/products.routes'
+import {connectDB} from './database';
+
+connectDB();
 
 app.use(morgan('dev'))
 
