09d6ea0b6ceea6a2812b56f7b827240bd8d2939b
Jose David <jose7312000@gmail.com>
2022-11-09 23:42:38 -0500

    Guardando el rol al usuario

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/User'
-import jwt from 'jsonwebtoken';
-import { CONFIGJWT } from '../config';
+import jwt from 'jsonwebtoken'
+import { CONFIGJWT } from '../config'
+import Role from '../models/Role'
 
 export const signUp = async (req, res) => {
     const { username, email, password, roles } = req.body;
@@ -11,7 +12,16 @@ export const signUp = async (req, res) => {
         password: await User.encryptPassword(password)
     })
 
+    if(roles){
+        const foundRoles = await roles.find({name: {$in: roles}})
+        newUser.roles = foundRoles.map(role => role._id)
+    }else{
+        const role = await Role.findOne({name: "user"})
+        newUser.roles = [role._id];
+    }
+
     const savedUser = await newUser.save();
+    console.log(savedUser)
 
     const token = jwt.sign({id: savedUser._id}, CONFIGJWT, {
         expiresIn: 86400//24 hours
